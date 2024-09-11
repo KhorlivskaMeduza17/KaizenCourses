41aec717f9a3292b9f7de63367b5b5dec57a24b8
Valeriia Novikova <lerumiu@gmail.com>
2024-09-11 11:51:53 +0200

    Add all files

---
+++ Kaizen.MainWeb/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kaizen.MainWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("/api/home")]
+        public IActionResult Get()
+        {
+            var value = new { id = 1 };
+            return Ok(value);
+        }
+    }
+}
